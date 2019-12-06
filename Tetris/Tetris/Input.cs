6f357e0b2eb6f4bd103f8173d1ab152f4bc7026e
Juho Moisio <juho.moisio@tuni.fi>
2019-12-06 21:43:53 +0200

    Input-class is now usable

--- Tetris/Tetris/Input.cs
+++ Tetris/Tetris/Input.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Windows.Forms;
 
 namespace Tetris
 {
     class Input
     {
+
+        //Load table of available keys
+        private static Hashtable keytbl = new Hashtable();
+
+        //Checks if a key is pressed atm
+        public static bool KeyPressed(Keys key)
+        {
+            if (keytbl[key] == null)
+            {
+                return false;
+            }   
+            return (bool)keytbl[key];
+        }
+
+        //Sets the active key's state
+        public static void ChangeState(Keys key, bool state)
+        {
+            keytbl[key] = state;
+        }
     }
 }
