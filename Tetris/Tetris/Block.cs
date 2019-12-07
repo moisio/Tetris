c70a9265c0eb9f5fb6fbfa640dbdc6e662a728e3
Juho Moisio <juho.moisio@tuni.fi>
2019-12-07 21:50:09 +0200

    Ending branch

---
+++ Tetris/Tetris/Block.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Block
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool Stop { get; set; }
+
+        public Block()
+        {
+            X = 0;
+            Y = 0;
+            Stop = false;
+        }
+
+    }
+}
